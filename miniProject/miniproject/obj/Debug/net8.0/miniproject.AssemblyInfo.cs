652e8befc6f299b0227514b7dbecdb104a7c536c
prabveer <81586936+prabveer@users.noreply.github.com>
2024-12-24 06:52:12 -0600

    worked on

--- miniProject/miniproject/obj/Debug/net8.0/miniproject.AssemblyInfo.cs
+++ miniProject/miniproject/obj/Debug/net8.0/miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6856cc3df84551bfed219f81033b1e9b8ce17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e78b27aa91486b260bdc677d5ae5b28f95f6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
